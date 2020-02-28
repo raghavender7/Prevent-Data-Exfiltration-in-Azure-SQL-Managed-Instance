f8887946c602c2941f862d8fdc558b7d2d13f688
Krishna Golla <raghavender7@gmail.com>
2020-02-28 05:37:22 -0500

    Delete SQLLinkedServerAuditingMI.sql

--- SQLLinkedServerAuditingMI.sql
+++
@@ -1,6 +0,0 @@
-USE [master]GOCREATE SERVER AUDIT [LinkedServer] TO EXTERNAL_MONITOR;
-GOCREATE DATABASE AUDIT SPECIFICATION [LinkedServerMaster]FOR SERVER AUDIT [LinkedServer]ADD (EXECUTE ON OBJECT::[sys].[sp_addlinkedserver] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_addserver] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_dropserver] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_addlinkedsrvlogin] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_droplinkedsrvlogin] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_serveroption] BY [dbo]),ADD (EXECUTE ON OBJECT::[sys].[sp_setnetname] BY [dbo])WITH (STATE = ON)GO
-
-ALTER SERVER AUDIT [LinkedServer] WITH (STATE=ON);
-GO
-
